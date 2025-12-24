5dcc7d3adeeda5f16cdbb0072233a3c422c89504
Harakesava <harakesava18@gmail.com>
2025-12-24 10:57:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,3 +39,10 @@ for i in range (x):
     for j in range (i+1):
         print ("*", end=" ")
     print (  )
+    
+#number pattern
+x=int(input("enter a number:"))
+for i in range (x):
+    for j in range (i+1):
+        print (j,end=" " )
+    print()

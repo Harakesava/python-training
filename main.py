5c344ec1f91dbd7f643aee6c2bc7079239bc11dc
Harakesava <harakesava18@gmail.com>
2025-12-24 12:12:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,15 @@ for i in range (x-2,-1,-1):
     for j in range (i+1):
         print ("*",end=' ')
     print()
-    
+
+#hallow square
+n=int(input("enter a number:"))
+for i in range (n):
+    for j in range (n):
+        if i==0 or j==0 or i==n-1 or j==n-1:
+            print ("*",end=' ')
+        else :
+            print (" ",end=" ")
+    print()
     
     

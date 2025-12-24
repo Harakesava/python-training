3077d15e224dc3a3dfa2a92362229feb3e750d86
Harakesava <harakesava18@gmail.com>
2025-12-24 12:21:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,5 +89,14 @@ for i in range (n):
         else :
             print (" ",end=" ")
     print()
-    
-    
+
+#hallow right angle tiangle
+n=int(input("enter a number:"))
+for i in range (n):
+    for j in range (i+1):
+        if i==n-1 or j==0 or i==j:
+            print ("*",end=' ')
+        else :
+            print (" ",end=" ")
+    print()
+

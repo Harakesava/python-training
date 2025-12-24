9bed72e5fa5548773562d0c17b3bbfa0ef8aa112
Harakesava <harakesava18@gmail.com>
2025-12-24 10:49:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,10 @@ for i in range (x):
     for j in range (x):
         print ("*",end=' ')
     print ()
+
+#right angle triangle pattern
+x=int(input("enter a number:"))
+for i in range (x):
+    for j in range (i+1):
+        print ("*", end=" ")
+    print (  )

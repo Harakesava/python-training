f5fe37f62df25c482d42dcafc9beb4c9a7d333b4
Harakesava <harakesava18@gmail.com>
2025-12-24 10:17:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ for i in range(1,x+1):
         print("fizz")
     else:
         print(i)
+
+#version 2 fizzbuzz problem
+x=int(input('enter the number:'))
+for i in range(1,x+1):
+    
+    if (i%3==0) :
+        if (i%5==0):
+        print("fizzbuzz")
+    elif (i%5==0):
+        print("buzz")
+    elif (i%3==0):
+        print("fizz")
+    else:
+        print(i)
